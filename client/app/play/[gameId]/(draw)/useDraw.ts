ded3079a2359663c2c096a58654db97f2084ebaf
Aayush Bhagat <lamblesus@gmail.com>
2023-11-07 12:09:11 -0500

    Created Drawing Board

--- client/app/play/[gameId]/(draw)/useDraw.ts
+++ client/app/play/[gameId]/(draw)/useDraw.ts
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from "react"
+import { useRef, useEffect, useState } from "react"
 
 export interface Draw{
   ctx: CanvasRenderingContext2D
@@ -12,20 +12,50 @@ type Point = {
 }
 
 export default function useDraw(onDraw: ({ctx, curPoint, prevPoint}: Draw) => void){
+  const [isMouseDown, setIsMouseDown] = useState(false)
   const canvasRef = useRef<HTMLCanvasElement | null>(null)
+  const prevPoint = useRef<null | Point>(null)
+
+
+  const onMouseDown = () => setIsMouseDown(true)
+
+  const clear = () => {
+    const canvas = canvasRef.current
+    if(!canvas) return 
+    
+    const ctx = canvas.getContext('2d')
+    if(!ctx) return
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+  }
+
+  const undo = () => {
+    const canvas = canvasRef.current
+
+    if (!canvas) {
+      return
+    }
+    
+    const ctx = canvas.getContext("2d")
+
+    if (!ctx) return
+    ctx.restore()
+  }
 
   useEffect(()=>{
     const mouseMoveHandler = (e: MouseEvent) =>{
+      if (!isMouseDown) return;
+
       const curCoords = getCanvasPoints(e)
 
       const ctx = canvasRef.current?.getContext('2d')
 
       if(!ctx || !curCoords) return
 
-
+      onDraw({ctx, curPoint: curCoords, prevPoint: prevPoint.current})
+      ctx.save()
+      prevPoint.current = curCoords
 
     }
-    canvasRef?.current?.addEventListener("mousemove", mouseMoveHandler)
 
     const getCanvasPoints = (e: MouseEvent) => {
       const canvas = canvasRef.current
@@ -40,11 +70,21 @@ export default function useDraw(onDraw: ({ctx, curPoint, prevPoint}: Draw) => vo
 
     }
 
+    canvasRef?.current?.addEventListener("mousemove", mouseMoveHandler)
+
+    const mouseUpHandler = ()=>{
+      setIsMouseDown(false)
+      prevPoint.current = null
+    }
+
+    window.addEventListener("mouseup", mouseUpHandler)
+
     //cleanup
     return () => {
       canvasRef.current?.removeEventListener("mousemove", mouseMoveHandler)
+      window.removeEventListener("mouseup", mouseUpHandler)
     } 
-  }, [])
+  }, [onDraw, isMouseDown])
 
-  return { canvasRef }
+  return { canvasRef, onMouseDown, clear, undo }
 }
